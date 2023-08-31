edd3c7fd942ba3154c96b0e8918ebb602c5d9fc4
vismay12195 <130983117+vismay12195@users.noreply.github.com>
2023-08-31 14:33:20 +0000

    Creating a function that looks through an array arr and returns the first element in it that passes a 'truth test'. This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.

---
+++ Truth_Test.js
@@ -0,0 +1,14 @@
+function truthTest(arr, func) {
+    let num = 0;
+    let truthArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        num = arr[i];
+        if (func(num)) {
+            console.log(num);
+        }
+    }
+    console.log(undefined);
+}
+
+truthTest([254, 24, 66, 33, 56], num => num % 2 === 0);
+truthTest([1, 3, 5, 9], function (num) { return num % 2 === 0; });
